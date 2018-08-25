24a2225a14adfa7c5be19f22e03661c9df607fad
KJai <ktcodergirl@gmail.com>
2018-08-25 13:07:34 -0700

    checkin the string permutation class

---
+++ src/PermutatonString.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutatonString {
+
+    public static void main(String[] args) {
+
+        String input = "abcd";
+
+        List<String> output = permute(input);
+
+        for (String out: output) {
+            System.out.print(out + " ");
+        }
+
+    }
+
+    public static List<String> permute(String input) {
+        List<String> output = new ArrayList<>();
+        permuteHelper(input,0,input.length() -1, output);
+        return output;
+    }
+
+
+    public static String swap(String input, int from, int to) {
+        char[] charArray = input.toCharArray();
+        char temp = charArray[from];
+        charArray[from] = charArray[to];
+        charArray[to] = temp;
+
+        return String.valueOf(charArray);
+    }
+
+    public static void permuteHelper(String input, int left, int right, List<String> output) {
+
+        if (left == right) {
+            output.add(input);
+        }
+
+        for (int i = left; i <= right; i++) {
+            input = swap(input,left,i);
+            permuteHelper(input, left+1,right, output);
+            input = swap(input,left,i); //backtrack
+        }
+
+    }
+
+}
